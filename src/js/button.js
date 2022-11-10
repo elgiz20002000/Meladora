a675f011daf048398da54b917f11623b11929dc5
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2022-11-10 11:01:48 +0400

    deploy

--- src/js/button.js
+++ src/js/button.js
@@ -5,9 +5,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.opacity = 1
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.opacity = 0
   }
 
   mybutton.addEventListener('click' , () =>  {
